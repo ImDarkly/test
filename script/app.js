5511c6cce993a6e8111dbde1fdd61dcdb5df8bfc
ImDarkly <66973381+ImDarkly@users.noreply.github.com>
2022-03-27 15:24:19 +0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1>All Pages</h1>
+
+            Привіт!!
+
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
